f4b5e98d19fe132e92c5eef14747d571c564083f
zeroest <husheart@naver.com>
2023-05-17 01:26:29 +0900

    State 변경사항에 코드 복잡도 증가

--- src/main/java/me/zeroest/designpattern/state/before/Client.java
+++ src/main/java/me/zeroest/designpattern/state/before/Client.java
@@ -4,6 +4,11 @@ public class Client {
     public static void main(String[] args) {
         Laptop laptop = new Laptop();
 
+        laptop.powerPush(); // 절전모드
+        laptop.powerPush(); // 전원 on
+        laptop.powerPush(); // 전원 off
+
+        laptop.powerPush(); // 절전모드
         laptop.powerPush(); // 전원 on
         laptop.powerPush(); // 전원 off
     }
--- src/main/java/me/zeroest/designpattern/state/before/Laptop.java
+++ src/main/java/me/zeroest/designpattern/state/before/Laptop.java
@@ -1,9 +1,17 @@
 package me.zeroest.designpattern.state.before;
 
+/*
+간단하게 켜고, 끄는 노트북에 절전모드를 추가
+상태에 따른 동작은 다음과 같다고 가정
+- 노트북 전원이 켜져 있는 상태에서 전원 버튼을 누르면, 전원을 끌 수 있습니다.
+- 노트북 전원이 꺼져 있는 상태에서 전원 버튼을 누르면, 절전모드가 됩니다.
+- 노트북 절전모드 상태에서 전원 버튼을 누르면, 전원을 켤 수 있습니다.
+*/
 public class Laptop {
 
     private static final String ON = "on";
     private static final String OFF = "off";
+    private static final String SAVING = "saving";
     private String powerState = "";
 
     public Laptop(){
@@ -15,12 +23,16 @@ public class Laptop {
     }
 
     public void powerPush(){
-        if ("on".equals(this.powerState)) {
+        if (ON.equals(this.powerState)) {
             setPowerState(Laptop.OFF);
             printState();
         }
-        else {
+        else if (SAVING.equals(this.powerState)) {
             setPowerState(Laptop.ON);
+            printState();
+        }
+        else {
+            setPowerState(Laptop.SAVING);
             printState();
         }
     }
